af182655f1f73fe113dc2a1f2a2205c88ca4d2dc
airs-upei <airs@upei.ca>
2024-04-20 04:03:33 -0300

    m

---
+++ app.R
@@ -0,0 +1,11 @@
+# library(MAST21V2)
+# library(shiny)
+# library(lme4)
+# library(psych)
+# library(itembankr)
+
+# deploy_MAST21_2024_v2(musicassessr_aws = FALSE, language = "en")
+
+# deploy_MAST21_2022(musicassessr_state = "test")
+
+deploy_MAST21V2_2024(musicassessr_state = "production", language = "en", final_qualtrics_url = "")
